8554e29e8c1a374fc57484de6d231608be7351dd
Delli <krisdimond@gmail.com>
2026-04-20 15:29:57 +0300

    Added user registration scheme

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,20 @@
-from sqlalchemy import create_all_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import time
 
-#Адреса бази
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db:5432/cosplay_db"
-
+#очікування поки хост підніметься
+for i in range(10):
+    try:
+        engine = create_engine(SQLALCHEMY_DATABASE_URL)
+        engine.connect()
+        print("Хост підключен")
+        break
+    except Exception as e:
+        time.sleep(2)
+else:
+    raise Exception("Не можливо підключитись до хосту")
 #Двигун
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from passlib.context import CryptContext
-from . import models
-from .database import engine
+import models
+import schemas
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 #автоматичне створення таблиць при запуску
 models.Base.metadata.create_all(bind=engine)
@@ -13,8 +15,24 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def read_root():
     return {"message": "Cosplay Manager API is running"}
 
-@app.post("/register")
-def register(password: str):
-    # Хешування пароля - те, що вимагав керівник
-    hashed = pwd_context.hash(password)
-    return {"hashed_password": hashed}
+#Тимчасове підключення до бд для кожного запиту
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#хешування паролю
+@app.post("/register", response_model=schemas.UserOut)
+def register(user_data: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user_data.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Введена поштова адреса вже використовується!")
+    hashed = pwd_context.hash(user_data.password)
+    new_user = models.User(email=user_data.email, hashed_password=hashed)
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
